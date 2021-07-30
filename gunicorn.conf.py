c37144fe411d9c2ae15f138664f9aced4464860e
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-07-30 11:55:11 +0200

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,7 @@
-bind = "0.0.0.0:5000"
-workers = 10
+bind = "127.0.0.1:5000"
+workers = 5
+threads = 5
+keepalive = 1
 worker_class = "gevent"
 keyfile = "/etc/letsencrypt/live/server.duinocoin.com/privkey.pem"
 certfile = "/etc/letsencrypt/live/server.duinocoin.com/fullchain.pem"
