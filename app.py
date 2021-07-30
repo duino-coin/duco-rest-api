92ce7ea75b9a2806c0a580b8299ef445bf55d721
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-07-30 11:55:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,25 +10,33 @@ from re import match
 from collections import OrderedDict
 from operator import itemgetter
 import traceback
+from fastrand import pcg32bounded as fastrandint
+from hashlib import sha1
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from re import sub
 from Server import (
+    now,
+    jail,
+    global_last_block_hash,
     DATABASE,
+    DUCO_PASS,
     DB_TIMEOUT,
     CONFIG_MINERAPI,
     CONFIG_TRANSACTIONS,
-    API_JSON_URI,
-    global_last_block_hash,
-    now,
-    SAVE_TIME
+    API_JSON_URI
 )
 from flask_caching import Cache
-
+from gevent import monkey
+monkey.patch_all()
+DB_TIMEOUT = 10
+SAVE_TIME = 10
 config = {
     "DEBUG": False,
     "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 5
+    "CACHE_DEFAULT_TIMEOUT": SAVE_TIME
 }
-DB_TIMEOUT = 5
-SAVE_TIME = 5
+
 app = Flask(__name__)
 app.config.from_mapping(config)
 cache = Cache(app)
@@ -42,11 +50,11 @@ last_balances_update = 0
 use_cache = True
 
 
-def _success(result, code: int = 200):
+def _success(result, code=200):
     return jsonify(success=True, result=result), code
 
 
-def _error(string, code=400):
+def _error(string, code=200):
     return jsonify(success=False, message=string), code
 
 
@@ -61,32 +69,182 @@ def row_to_transaction(row):
     }
 
 
+@app.route("/transaction/")
+def api_transaction():
+    username = request.args.get('username', None)
+    unhashed_pass = request.args.get('password', None).encode('utf-8')
+    recipient = request.args.get('recipient', None)
+    amount = request.args.get('amount', None)
+    memo = request.args.get('memo', None)
+
+    try:
+        with sqlconn(DATABASE, timeout=DB_TIMEOUT) as conn:
+            # print(f'fetching user from {DATABASE}')
+            # User exists, read his password
+            datab = conn.cursor()
+            datab.execute(
+                """SELECT *
+                    FROM Users
+                    WHERE username = ?""",
+                (str(username),))
+            stored_password = datab.fetchone()[1]
+
+        try:
+            if not checkpw(unhashed_pass, stored_password):
+                return _error("Invalid password")
+        except:
+            if not checkpw(unhashed_pass, stored_password.encode('utf-8')):
+                return _error("Invalid password")
+    except Exception as e:
+        return _error("No user found: " + str(e))
+
+    try:
+        random = fastrandint(1000)
+        global_last_block_hash_cp = sha1(
+            bytes(str(username)+str(amount)+str(random),
+                  encoding='ascii')).hexdigest()
+        memo = sub(r'[^A-Za-z0-9 .()-:/!#_+-]+', ' ', str(memo))
+
+        if memo == "-" or memo == "":
+            memo = "None"
+
+        if str(recipient) == str(username):
+            return _error("NO,You\'re sending funds to yourself")
+
+        if (str(amount) == "" or float(amount) <= 0):
+            return _error("NO,Incorrect amount")
+
+        with sqlconn(DATABASE,
+                     timeout=DB_TIMEOUT) as conn:
+            datab = conn.cursor()
+            datab.execute(
+                """SELECT *
+                        FROM Users
+                        WHERE username = ?""",
+                (username,))
+            balance = float(datab.fetchone()[3])
+
+        if (float(balance) <= float(amount)):
+            return _error("NO,Incorrect amount")
+
+        with sqlconn(DATABASE,
+                     timeout=DB_TIMEOUT) as conn:
+            datab = conn.cursor()
+            datab.execute(
+                """SELECT *
+                    FROM Users
+                    WHERE username = ?""",
+                (recipient,))
+            recipientbal = float(datab.fetchone()[3])
+
+        if float(balance) >= float(amount):
+            balance -= float(amount)
+            recipientbal += float(amount)
+
+            while True:
+                try:
+                    with sqlconn(DATABASE,
+                                 timeout=DB_TIMEOUT) as conn:
+                        datab = conn.cursor()
+                        datab.execute(
+                            """UPDATE Users
+                            set balance = ?
+                            where username = ?""",
+                            (balance, username))
+                        datab.execute(
+                            """UPDATE Users
+                            set balance = ?
+                            where username = ?""",
+                            (round(float(recipientbal), 20), recipient))
+                        conn.commit()
+                        break
+                except:
+                    pass
+
+            formatteddatetime = now().strftime("%d/%m/%Y %H:%M:%S")
+            with sqlconn(CONFIG_TRANSACTIONS,
+                         timeout=DB_TIMEOUT) as conn:
+                datab = conn.cursor()
+                datab.execute(
+                    """INSERT INTO Transactions
+                    (timestamp, username, recipient, amount, hash, memo)
+                    VALUES(?, ?, ?, ?, ?, ?)""",
+                    (formatteddatetime,
+                        username,
+                        recipient,
+                        amount,
+                        global_last_block_hash_cp,
+                        memo))
+                conn.commit()
+
+            print("Successfully transferred", amount, "from",
+                  username, "to", recipient, global_last_block_hash_cp)
+            return _success("OK,Successfully transferred funds,"
+                            + str(global_last_block_hash_cp))
+    except Exception as e:
+        return _success(
+            "NO,Internal server error: "
+            + str(traceback.format_exc()))
+
+
+@app.route("/auth/")
+@cache.cached()
+def api_auth():
+    username = request.args.get('username', None)
+    unhashed_pass = request.args.get('password', None).encode('utf-8')
+
+    try:
+        with sqlconn(DATABASE, timeout=DB_TIMEOUT) as conn:
+            # print(f'fetching user from {DATABASE}')
+            # User exists, read his password
+            datab = conn.cursor()
+            datab.execute(
+                """SELECT *
+                    FROM Users
+                    WHERE username = ?""",
+                (str(username),))
+            stored_password = datab.fetchone()[1]
+
+        try:
+            if unhashed_pass.decode() == DUCO_PASS:
+                return _success("Logged in")
+            elif checkpw(unhashed_pass, stored_password):
+                return _success("Logged in")
+            else:
+                return _error("Invalid password")
+        except:
+            if checkpw(unhashed_pass, stored_password.encode('utf-8')):
+                return _success("Logged in")
+            else:
+                return _error("Invalid password")
+    except Exception as e:
+        return _error("No user found: " + str(e))
+
+
 def get_all_transactions():
     global last_transactions_update
     global transactions
+    # return [] why???
 
     now = time()
-    if now - last_balances_update < SAVE_TIME*2:
-        pass
-        # print(f'returning a copy of transactions')
-    else:
+    if now - last_transactions_update >= SAVE_TIME:
         try:
             with sqlconn(CONFIG_TRANSACTIONS, timeout=DB_TIMEOUT) as conn:
-                print(f'fetching transactions from {DATABASE}')
+                # print(f'fetching transactions from {CONFIG_TRANSACTIONS}')
                 datab = conn.cursor()
                 datab.execute("SELECT * FROM Transactions")
                 rows = datab.fetchall()
-                print(f'done fetching transactions from {DATABASE}')
 
-            transactions = {}
-            for row in rows:
-                transactions[row[4]] = row_to_transaction(row)
+                transactions = {}
+                for row in rows:
+                    transactions[row[4]] = row_to_transaction(row)
 
-            last_transactions_update = time()
-        except:
+                last_transactions_update = time()
+        except Exception as e:
+            print(traceback.format_exc())
             pass
 
-    return transactions
+    return transactions.copy()
 
 
 def get_transactions(username: str):
@@ -96,7 +254,7 @@ def get_transactions(username: str):
     user_transactions = []
     for transaction in transactions:
         if (transactions[transaction]["sender"] == username
-            or transactions[transaction]["recipient"] == username):
+                or transactions[transaction]["recipient"] == username):
             user_transactions.append(transactions[transaction])
 
     return user_transactions
@@ -120,36 +278,37 @@ def row_to_miner(row):
 def get_all_miners():
     global last_miners_update
     global miners
-
+    # return []
     now = time()
-    if now - last_miners_update < SAVE_TIME*2:
+    if now - last_miners_update < SAVE_TIME:
         pass
         # print(f'returning a copy of miners')
     else:
         try:
             with sqlconn(CONFIG_MINERAPI, timeout=DB_TIMEOUT) as conn:
-                print(f'fetching miners from {DATABASE}')
+                # print(f'fetching miners from {DATABASE}')
                 datab = conn.cursor()
+
                 datab.execute("SELECT * FROM Miners")
                 rows = datab.fetchall()
-                print(f'done fetching miners from {DATABASE}')
+                # print(f'done fetching miners from {CONFIG_MINERAPI}')
+                miners_cp = {}
+                for row in rows:
+                    if not row[1] in miners_cp:
+                        miners_cp[row[1]] = []
+                    miners_cp[row[1]].append(row_to_miner(row))
+                miners = miners_cp
+                # print(f'done creating miner dict from {CONFIG_MINERAPI}')
 
-            miners = {}
-            for row in rows:
-                try:
-                    miners[row[1]].append(row_to_miner(row))
-                except:
-                    miners[row[1]] = []
-                    miners[row[1]].append(row_to_miner(row))
-
-            last_miners_update = time()
+                last_miners_update = time()
         except:
             pass
 
-    return miners
+    return miners.copy()
 
 
 @app.route("/miners/<username>")
+# @limiter.limit(60/minute)
 @cache.cached()
 def get_miners_api(username: str):
     # Get all miners
@@ -158,6 +317,7 @@ def get_miners_api(username: str):
         return _success(miners[username])
     except:
         return _error("No miners detected on that account")
+
 
 def get_miners(username: str):
     # For /users/
@@ -177,16 +337,16 @@ def get_all_balances():
     global balances
 
     now = time()
-    if now - last_balances_update < SAVE_TIME*3:
+    if now - last_balances_update < SAVE_TIME:
         pass
         # print(f'returning a copy of balances')
     else:
         with sqlconn(DATABASE, timeout=DB_TIMEOUT) as conn:
-            print(f'fetching balances from {DATABASE}')
+            # print(f'fetching balances from {DATABASE}')
             datab = conn.cursor()
             datab.execute("SELECT * FROM Users")
             rows = datab.fetchall()
-            print(f'done fetching balances from {DATABASE}')
+            # print(f'done fetching balances from {DATABASE}')
 
         balances = {}
         for row in rows:
@@ -194,7 +354,7 @@ def get_all_balances():
 
         last_balances_update = time()
 
-    return balances
+    return balances.copy()
 
 
 def get_balance(username: str):
@@ -207,6 +367,7 @@ def get_balance(username: str):
 
 
 @app.route("/users/<username>")
+# @limiter.limit(60/minute)
 @cache.cached()
 def api_get_user_objects(username: str):
     try:
@@ -234,6 +395,7 @@ def api_get_user_objects(username: str):
 
 
 @app.route("/transactions/<hash>")
+# @limiter.limit(60/minute)
 @cache.cached()
 def get_transaction_by_hash(hash: str):
     # Get all transactions
@@ -242,12 +404,13 @@ def get_transaction_by_hash(hash: str):
         for transaction in transactions:
             if transactions[transaction]["hash"] == hash:
                 return _success(transactions[transaction])
-        return _success("No transaction found")
+        return _error("No transaction found")
     except Exception as e:
-        return _error("This transaction doesn't exist")
+        return _error("No transaction found")
 
 
 @app.route("/balances/<username>")
+# @limiter.limit(60/minute)
 @cache.cached()
 def api_get_user_balance(username: str):
     try:
@@ -255,7 +418,9 @@ def api_get_user_balance(username: str):
     except Exception as e:
         return _error("This user doesn't exist")
 
+
 @app.route("/balances")
+# @limiter.limit(60/minute)
 @cache.cached()
 def api_get_all_balances():
     try:
@@ -265,6 +430,7 @@ def api_get_all_balances():
 
 
 @app.route("/transactions")
+# @limiter.limit(60/minute)
 @cache.cached()
 def api_get_all_transactions():
     try:
@@ -274,15 +440,18 @@ def api_get_all_transactions():
 
 
 @app.route("/miners")
+# @limiter.limit(60/minute)
 @cache.cached()
 def api_get_all_miners():
     try:
         return _success(get_all_miners())
     except Exception as e:
+        print(str(e))
         return _error("Error fetching miners: " + str(e))
 
 
 @app.route("/statistics")
+# @limiter.limit(60/minute)
 @cache.cached()
 def get_api_data():
     data = {}
