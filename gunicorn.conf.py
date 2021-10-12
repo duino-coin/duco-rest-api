63e73270fbd1e6c1fa92f52ef367296426d4273f
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2021-10-12 15:44:46 +0200

    VPS code update

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,13 @@
-bind = "127.0.0.1:5000"
-workers = 5
-threads = 5
-keepalive = 1
-worker_class = "gevent"
+from multiprocessing import cpu_count
+
+bind = "0.0.0.0:5000"
 keyfile = "/etc/letsencrypt/live/server.duinocoin.com/privkey.pem"
 certfile = "/etc/letsencrypt/live/server.duinocoin.com/fullchain.pem"
+
+worker_class = "gevent"
+workers = 6
+backlog = 0
+
+graceful_timeout = 5
+timeout = 600
+limit_request_line = 7777
